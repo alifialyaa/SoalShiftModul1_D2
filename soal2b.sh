f2f89904106a211b9bb1e2507c874b0145b84d58
17-193VaniaC <42793858+17-193VaniaC@users.noreply.github.com>
2019-03-02 00:22:58 +0700

    revisi

--- soal2b.sh
+++ soal2b.sh
@@ -1,2 +1,5 @@
-awk -F"," '{if ($1 == "United States") arr[$4] += $10 } END { for ( i in arr ) { print arr[i], i } }' WA_Sales_Products_2012-14.csv | sort -n | tail -n3 > laporan2_b.csv
+#!/usr/bin/awk
+
+awk -F"," '{if ($7 == 2012 && $1 == "United States") arr[$4] += $10 } END { for ( i in arr ) { print arr[i], i } }' WA_Sales_Products_2012-14.csv | sort -n | tail -n3 > laporan2_b.csv
+
 
--- soal2c.sh
+++ soal2c.sh
@@ -1,2 +1,3 @@
-awk -F"," '{if ($4 == "Mountaineering Equipment" || $4 == "Camping Equipment" || $4 == "Personal Accessories" ) arr[$6] += $10 } END { for ( i in arr ) { print arr[i], i } }' WA_Sales_Products_2012-14.csv | sort -n | tail -n3 > laporan2_c.csv
+#!/usr/bin/awk
+awk -F"," '{if ($1 == "United States" && $7 == 2012 && $4 == "Mountaineering Equipment" || $4 == "Camping Equipment" || $4 == "Personal Accessories" ) arr[$6] += $10 } END { for ( i in arr ) { print arr[i], i } }' WA_Sales_Products_2012-14.csv | sort -n | tail -n3 > laporan2_c.csv
 
